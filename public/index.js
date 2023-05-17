484dc7cd43075ef32e77ab04600c56645f0f515a
w3project <adityabatgeri@gmail.com>
2023-05-18 00:04:48 +0530

    pc version started

--- public/index.js
+++ public/index.js
@@ -31,3 +31,19 @@ function selectC()
     optionB.style.backgroundColor = "white";
     optionB.style.border = "1px solid rgba(0,0,0,0.3)";
 }
+
+function redirect()
+{
+    if (optionA.style.backgroundColor == "rgba(156, 119, 248, 0.1)")
+    {
+        window.location.href = "/login.html";
+    }
+    else if (optionB.style.backgroundColor == "rgba(156, 119, 248, 0.1)")
+    {
+        window.location.href = "/main_page.html";
+    }
+    else 
+    {
+        window.location.href = "/admin.html";
+    }
+}
