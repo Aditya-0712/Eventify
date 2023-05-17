715f402c45a66d007b9bd5e2127d8ed0ecbd2359
w3project <adityabatgeri@gmail.com>
2023-05-17 19:25:38 +0530

    UNDOABLE changes pt2

---
+++ public/index.js
@@ -0,0 +1,33 @@
+var optionA = document.getElementsByClassName("options")[0];
+var optionB = document.getElementsByClassName("options")[1];
+var optionC = document.getElementsByClassName("options")[2];
+
+function selectA()
+{
+    optionA.style.backgroundColor = "rgba(156,119,248,0.1)";
+    optionA.style.border = "1px solid rgba(27,27,175)";
+    optionB.style.backgroundColor = "white";
+    optionB.style.border = "1px solid rgba(0,0,0,0.3)";
+    optionC.style.backgroundColor = "white";
+    optionC.style.border = "1px solid rgba(0,0,0,0.3)";
+}
+
+function selectB()
+{
+    optionB.style.backgroundColor = "rgba(156,119,248,0.1)";
+    optionB.style.border = "1px solid rgba(27,27,175)";
+    optionA.style.backgroundColor = "white";
+    optionA.style.border = "1px solid rgba(0,0,0,0.3)";
+    optionC.style.backgroundColor = "white";
+    optionC.style.border = "1px solid rgba(0,0,0,0.3)";
+}
+
+function selectC()
+{
+    optionC.style.backgroundColor = "rgba(156,119,248,0.1)";
+    optionC.style.border = "1px solid rgba(27,27,175)";
+    optionA.style.backgroundColor = "white";
+    optionA.style.border = "1px solid rgba(0,0,0,0.3)";
+    optionB.style.backgroundColor = "white";
+    optionB.style.border = "1px solid rgba(0,0,0,0.3)";
+}
